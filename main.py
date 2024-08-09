ea3d63f827648624b56528da5d471654da7dfa85
Moneesh FMX <moneesh.kumar@fehrmann-materialsx.com>
2024-08-09 10:57:55 +0200

    Added main file for lint test

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a buggy Python script with style and linting issues
+
+import os,sys
+
+def my_function(x, y ):
+  print( "The sum of x and y is: " + str( x + y ))
+
+  for i in range(0, 10 ):
+      print(i )
+
+  if x>10 :
+   print("X is greater than 10")
+
+  if y== 20:print("Y is equal to 20")
+
+  unused_variable = 100
+
+my_function( 5,20)
