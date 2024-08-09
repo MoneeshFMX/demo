7dbf890a4419f189dbdcbac007a466f12a6a2c39
Moneesh FMX <moneesh.kumar@fehrmann-materialsx.com>
2024-08-09 11:30:17 +0200

    Resolved linting

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-# This is a buggy Python script with style and linting issues
-
-import os,sys
-
-def my_function(x, y ):
-  print( "The sum of x and y is: " + str( x + y ))
+def my_function(x: int, y:int) -> None:
+  print( "The sum of x and y is: " + str(x + y))
 
   for i in range(0, 10 ):
       print(i )
@@ -11,9 +7,8 @@ def my_function(x, y ):
   if x>10 :
    print("X is greater than 10")
 
-  if y== 20:print("Y is equal to 20")
+  if y== 20:
+    print("Y is equal to 20")
 
-  unused_variable = 100
-  random_num = 50
+my_function(5, 20)
 
-my_function( 5,20)
